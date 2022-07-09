de4f8998b089d39220c3fc646359e6dc2b896dbf
Jonas Wolfram <jonas.wolfram@me.com>
2022-07-09 09:37:49 +0200

    updates

--- FileSystem/fs-create-appendFile.js
+++ FileSystem/fs-create-appendFile.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.appendFile("fs-appendFile-file.txt", "H E U R E K A ! ! !", function (err) {
+fs.appendFile("fs-appendFile-file.txt", " H E U R E K A ! ! !", function (err) {
   if (err) throw err;
   console.log("Saved!");
 });
